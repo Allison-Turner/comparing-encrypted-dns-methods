dd99cb9a44c3a807d3e963b4d80b8c3330d2cd19
Allison Turner <aturner@ucsd.edu>
2022-05-26 17:10:15 -0700

    Update run-encrypted-dns-tests.sh

--- run-encrypted-dns-tests.sh
+++ run-encrypted-dns-tests.sh
@@ -3,15 +3,20 @@
 FILE_OWNER=$1
 
 HELPER_LOG="./helper.log"
+
+DOT_PCAP_DIR="./dot-pcaps"
+DOT_OUTFILE_DIR="./dot-outfiles"
+
 DOH_PCAP_DIR="./doh-pcaps"
-DOQ_PCAP_DIR="./doq-pcaps"
 DOH_OUTFILE_DIR="./doh-outfiles"
+
+DOQ_PCAP_DIR="./doq-pcaps"
 DOQ_OUTFILE_DIR="./doq-outfiles"
 
 TS=$(date +"%Y-%m-%d-%H-%M-%S")
 
-#declare -a domains=("google.com" "bing.com" "ucsd.edu" "cdc.gov" "fda.gov" "louvre.fr" "narendramodi.in" "bu.edu" "uw.edu" "af.mil" "marines.mil" "army.mil" "npr.org")
-declare -a domains=("google.com")
+declare -a domains=("google.com" "bing.com" "ucsd.edu" "cdc.gov" "fda.gov" "louvre.fr" "narendramodi.in" "bu.edu" "uw.edu" "af.mil" "marines.mil" "army.mil" "npr.org")
+#declare -a domains=("google.com")
 
 init(){
     if [ ! -f "$HELPER_LOG" ]; then
@@ -19,19 +24,29 @@ init(){
         chown $FILE_OWNER $HELPER_LOG
     fi
 
+    if [ ! -d "$DOT_PCAP_DIR" ]; then
+        mkdir "$DOT_PCAP_DIR"
+        chown $FILE_OWNER $DOT_PCAP_DIR
+    fi
+
+    if [ ! -d "$DOT_OUTFILE_DIR" ]; then
+        mkdir "$DOT_OUTFILE_DIR"
+        chown $FILE_OWNER $DOT_OUTFILE_DIR
+    fi
+
     if [ ! -d "$DOH_PCAP_DIR" ]; then
         mkdir "$DOH_PCAP_DIR"
         chown $FILE_OWNER $DOH_PCAP_DIR
     fi
 
-    if [ ! -d "$DOQ_PCAP_DIR" ]; then
-        mkdir "$DOQ_PCAP_DIR"
-        chown $FILE_OWNER $DOQ_PCAP_DIR
-    fi
-
     if [ ! -d "$DOH_OUTFILE_DIR" ]; then
         mkdir "$DOH_OUTFILE_DIR"
         chown $FILE_OWNER $DOH_OUTFILE_DIR
+    fi
+
+    if [ ! -d "$DOQ_PCAP_DIR" ]; then
+        mkdir "$DOQ_PCAP_DIR"
+        chown $FILE_OWNER $DOQ_PCAP_DIR
     fi
 
     if [ ! -d "$DOQ_OUTFILE_DIR" ]; then
@@ -41,7 +56,7 @@ init(){
 }
 
 start_packet_capture(){
-    FILE=$(dumpcap -i docker0 --autostop duration:10 -n -q 2>&1 >/dev/null | grep "File:" | awk '{print $2}')
+    FILE=$(dumpcap -i docker0 --autostop duration:15 -n -q 2>&1 >/dev/null | grep "File:" | awk '{print $2}')
     echo $FILE > $HELPER_LOG
 }
 
@@ -74,6 +89,33 @@ move_pcap_to_result_dir(){
     wait
 }
 
+run_dot_test(){
+    DOMAIN=$1
+    TYPE=$2
+    TIMESTAMP=$3
+    OUTFILE="$DOT_OUTFILE_DIR/$DOMAIN-$TYPE-$TIMESTAMP.out"
+
+    echo "DOT: '$TYPE' record query on domain '$DOMAIN'"
+
+    touch $OUTFILE
+
+    CONTAINER_ID=$(docker run -dit adguard-dnslookup-env)
+    wait
+
+    sleep 5
+    start_packet_capture &
+    sleep 2
+    docker exec $CONTAINER_ID /bin/bash -c "export RRTYPE=$TYPE && ./dnslookup $DOMAIN tls://dns.adguard.com" >> $OUTFILE 2>&1
+    wait
+    chown $FILE_OWNER $OUTFILE
+
+    remove_test_container $CONTAINER_ID
+
+    move_pcap_to_result_dir $DOT_PCAP_DIR 'dot' $i $TYPE $TIMESTAMP
+
+    echo "Test finished"
+}
+
 run_doh_test(){
     DOMAIN=$1
     TYPE=$2
@@ -84,12 +126,12 @@ run_doh_test(){
 
     touch $OUTFILE
 
-    CONTAINER_ID=$(docker run -dit doh-env)
+    CONTAINER_ID=$(docker run -dit curl-cloudflare-resolver-env)
     wait
 
-    sleep 1
+    sleep 5
     start_packet_capture &
-    sleep 1
+    sleep 2
     docker exec $CONTAINER_ID curl -H 'accept: application/dns-json' 'https://cloudflare-dns.com/dns-query?name='$DOMAIN'&type='$TYPE >> $OUTFILE 2>&1
     wait
     chown $FILE_OWNER $OUTFILE
@@ -111,12 +153,12 @@ run_doq_test(){
 
     touch $OUTFILE
 
-    CONTAINER_ID=$(docker run -dit doq-env)
+    CONTAINER_ID=$(docker run -dit adguard-dnslookup-env)
     wait
 
-    sleep 1
+    sleep 5
     start_packet_capture &
-    sleep 1
+    sleep 2
     docker exec $CONTAINER_ID /bin/bash -c "export RRTYPE=$TYPE && ./dnslookup $DOMAIN quic://dns.adguard.com" >> $OUTFILE 2>&1
     wait
     chown $FILE_OWNER $OUTFILE
@@ -134,12 +176,18 @@ wait
 docker stop $(docker ps -a -q)
 wait
 
-docker build -t doh-env -f doh-env.Dockerfile .
-docker build -t doq-env -f doq-env.Dockerfile .
+docker build -t curl-cloudflare-resolver-env -f cloudflare-rest-api-via-curl-env.Dockerfile .
+docker build -t adguard-dnslookup-env -f adguard-dnslookup-env.Dockerfile .
 wait
 
 for i in "${domains[@]}"
 do
+    run_dot_test $i 'A' $TS
+    wait
+
+    run_dot_test $i 'AAAA' $TS
+    wait
+
     run_doh_test $i 'A' $TS
     wait
 
